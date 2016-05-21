0c978fbfd6f08b00647b625b6b10e1d1c79a58d8
Andriy Oblivantsev <eslider@gmail.com>
2016-05-21 13:43:44 +0200

    Define RequestTest namespace

--- Tests/RequestTest.php
+++ Tests/RequestTest.php
@@ -1,5 +1,5 @@
 <?php
-include "../vendor/autoload.php";
+namespace WFS\Tests;
 
 use WFS\Component\Driver;
 use WFS\Entity\FeatureTypeList;
@@ -9,15 +9,17 @@ use WFS\Entity\ServiceProvider;
 /**
  *
  */
-class RequestTest
+class RequestTest extends \PHPUnit_Framework_TestCase
 {
     /**
      * RequestTest constructor.
      */
     public function __construct()
     {
-        $driver          = new Driver("http://demo.boundlessgeo.com/geoserver/wfs");
-        $caps            = $driver->convertXmlToSimpleArray(file_get_contents("../Tests/WFS/2.0.0/GetCapabilities.xml"));
+        $capabilitiesXML = "Tests/WFS/2.0.0/GetCapabilities.xml";
+        $wfsURL          = "http://demo.boundlessgeo.com/geoserver/wfs";
+        $driver          = new Driver($wfsURL);
+        $caps            = $driver->convertXmlToSimpleArray(file_get_contents($capabilitiesXML));
         $operations      = new OperationsMetadata($caps['ows_OperationsMetadata']);
         $serviceProvider = new ServiceProvider($caps["ows_ServiceProvider"]);
         $featureTypeList = new FeatureTypeList($caps['FeatureTypeList']);
@@ -29,11 +31,6 @@ class RequestTest
 
         //$desc = $driver->xml2array(file_get_contents("../tests/wfs/2.0.0/DescribeFeatureType.xml"));
         //$result = $driver->query('GetCapabilities');
-        $version = "2.0.0";
-        //$testXmlPath      = "wfs/{$version}/GetCapabilities.xml";
-        //$simpleXMLElement = simplexml_load_file($testXmlPath);
-        //$nameSpaceUrls    = $simpleXMLElement->getDocNamespaces(true);
-        //$r                = array($simpleXMLElement->getName() => $this->xml2array($simpleXMLElement));
     }
 
     public function testGetFeature()
@@ -44,9 +41,10 @@ class RequestTest
         //&request=GetFeature
         //&typename=osm:water_areas
         //&outputFormat=text/javascript
-        //&format_options=callback:loadFeatures&srsname=EPSG:3857&bbox=-8922952.933898335,5361598.912035402,-8913168.994277833,5371382.851655904,EPSG:3857&_=1463585466550
+        //&format_options=callback:loadFeatures
+        //&srsname=EPSG:3857
+        //&bbox=-8922952.933898335,5361598.912035402,-8913168.994277833,5371382.851655904,EPSG:3857
+        //&_=1463585466550
     }
-
 }
 
-new RequestTest();
